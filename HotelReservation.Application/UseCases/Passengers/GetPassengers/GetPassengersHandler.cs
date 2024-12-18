5447d78146dd5a09113272ba465f99abf2f14a18
Bmez <daniel.bmez1@gmail.com>
2024-12-18 16:19:03 -0500

    Fix: Updated Response wtih enums

---
+++ HotelReservation.Application/UseCases/Passengers/GetPassengers/GetPassengersHandler.cs
@@ -0,0 +1,28 @@
+﻿using Common;
+
+using HotelReservation.Application.Interfaces;
+using HotelReservation.Application.UseCases.Passengers.Dtos;
+
+using MediatR;
+
+namespace HotelReservation.Application.UseCases.Passengers.GetPassengers;
+
+public class GetPassengersHandler(IPassengerRepository passengerRepository) : IRequestHandler<GetPassengersQuery, Result<IEnumerable<PassengerResponseDto>>>
+{
+    public async Task<Result<IEnumerable<PassengerResponseDto>>> Handle(GetPassengersQuery request, CancellationToken cancellationToken)
+    {
+        var passengers = await passengerRepository.ListAsync();
+        var passengerDtos = passengers.Select(p => new PassengerResponseDto(
+            p.Id,
+            p.FullName,
+            p.DateOfBirth,
+            p.Gender.ToString(),
+            p.DocumentType.ToString(),
+            p.DocumentNumber,
+            p.Email,
+            p.PhoneNumber
+            ));
+
+        return Result.Success(passengerDtos, totalData: passengerDtos.Count());
+    }
+}
---
+++ HotelReservation.Application/UseCases/Passengers/GetPassengers/GetPassengersQuery.cs
@@ -0,0 +1,9 @@
+﻿using Common;
+
+using HotelReservation.Application.UseCases.Passengers.Dtos;
+
+using MediatR;
+
+namespace HotelReservation.Application.UseCases.Passengers.GetPassengers;
+
+public record GetPassengersQuery : IRequest<Result<IEnumerable<PassengerResponseDto>>>;
